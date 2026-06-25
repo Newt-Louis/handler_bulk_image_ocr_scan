304f029a955bd8cf222a54d286bd98cfc8fc586f
ngoc-le <lengoc@thiso.vn>
2026-06-25 23:00:58 +0700

    Chore: Remove build directory from tracking and update gitignore

--- build/CMakeFiles/autophoto.dir/build/.rcc/qrc_qmake_AutoPhoto.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/autophoto.dir/build/.rcc/qrc_qmake_AutoPhoto.cpp.o: \
- /home/newt-louis/workspaces/software_projects/hanled_image_ocr_scan/build/.rcc/qrc_qmake_AutoPhoto.cpp \
- /usr/include/stdc-predef.h
